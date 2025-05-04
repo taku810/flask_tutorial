5192fd7019301e7cca9b73c743d50d8f3ceff841
takumi <taku810616@gmail.com>
2025-05-04 12:22:39 +0900

    初期コミット

--- models/models.py
+++ models/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class OnegaiContent(Base):
     __tablename__ = "onegaicontents"
     id = Column(Integer, primary_key = True)
-    title = Column(String(128), unique=True)
+    title = Column(String(128))  # unique=Trueを削除
     body = Column(Text)
     date = Column(DateTime, default = datetime.now())
 
